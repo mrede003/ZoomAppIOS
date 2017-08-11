d2f53e93e118f51aa8d169411bf7cd8d44dc3105
Matthew Redenius <matt-mac@matthews-mini.hitronhub.home>
2017-08-10 18:52:35 -0700

    -added code to pull down company object from firebase

--- Zoom/ViewController.swift
+++ Zoom/ViewController.swift
@@ -11,13 +11,23 @@ import Firebase
 
 class ViewController: UIViewController {
 
+    var ref: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference().child("company").child("zoom")
+        let word = "hello"
+        print(word)
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            
+            let twitter_username = value?["twitter_username"] as? String ?? ""
+            print(twitter_username)
+            
+        })
         //begin to populate the database
         //load the initial screen
         //add listeners?
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
