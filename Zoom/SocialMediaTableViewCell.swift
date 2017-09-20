33f2298768054b1aa6cb106eca93ad1e41606cb3
Matthew Redenius <mrede003@gmail.com>
2017-09-19 18:57:56 -0700

    Finished Basic UI layout.

---
+++ Zoom/SocialMediaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SocialMediaTableViewCell.swift
+//  Zoom
+//
+//  Created by Matthew Redenius on 9/19/17.
+//  Copyright © 2017 Matt Redenius. All rights reserved.
+//
+
+import UIKit
+
+class SocialMediaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
