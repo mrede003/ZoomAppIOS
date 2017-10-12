abcd1430b254eff1d13b9b825c2ff85042bfb4c4
Matthew Redenius <mrede003@gmail.com>
2017-10-11 17:58:09 -0700

    -Added logic to make scrollable area larger when keyboard is present -Added logic to make keyboard disappear on tap outside of UITextView

--- Zoom/AppointmentController.swift
+++ Zoom/AppointmentController.swift
@@ -14,11 +14,34 @@ class AppointmentController: UIViewController {
     var companyObj: Company?
     var storeList: Stores?
     
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var areaCodeNumber: UITextField!
+    @IBOutlet weak var datePicker: UITextField!
+    @IBOutlet weak var lastNumber: UITextField!
+    @IBOutlet weak var middleNumber: UITextField!
+    @IBOutlet weak var timePicker: UITextField!
+    @IBOutlet weak var locationPicker: UITextField!
+    @IBOutlet weak var repPicker: UITextField!
+    @IBOutlet weak var descriptionForVisit: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func requestAppointmentButton(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //self.view.makeToast("This is a piece of toast", duration: 3.0, position: .center)
+        
+        //Set Observers to watch for keyboard appearence
+        registerKeyboardNotifications()
+        
+        //Add Gesture Recognizer to hide keyboard when a tap is made outside of a textview
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
+        
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,6 +49,38 @@ class AppointmentController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func sendEmail(message : String) {
+        
+    }
+    
+    func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillShow,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillHide,
+                                               object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let userInfo: NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardInfo = userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue
+        let keyboardSize = keyboardInfo.cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset = .zero
+        scrollView.scrollIndicatorInsets = .zero
+    }
 
     /*
     // MARK: - Navigation
